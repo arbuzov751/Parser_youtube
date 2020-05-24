e6fd1ef3b31f20bed1905a74f2198efc35a1a1c8
arbuzov751 <arbuzov751@rambler.ru>
2020-05-24 17:34:54 +0300

    some fixes

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import csv
+import glob
 
 
 def csv_dict_reader(file_obj):
@@ -10,9 +11,25 @@ def csv_dict_reader(file_obj):
     for line in reader:
         print(line["url"])
         print(line["directory_name"])
-        YouTube(line["url"]).streams.first().download(f'{line["directory_name"]}')
+        direct = line["directory_name"]
+        files_in_direct = glob.glob(f"{direct}\*.*")
+        try:
+            yt = YouTube(line["url"]).streams.first()
+            file_name = yt.title
+            ext = yt.mime_type.split('/')[1]
+
+            file_name = file_name.replace(".", "")
+            file_name = file_name.replace(":", "")
+
+            file_name = direct + "\\" + file_name + "." + ext
+            if file_name not in files_in_direct:
+                yt.download(f'{line["directory_name"]}')
+        except:
+            pass
+            # Нужно ли делать перезапись файла, чтоб пользователь увидел ошибку?
+            # line["directory_name"] = "Невозможно скачать файл!"
 
 
 if __name__ == "__main__":
-    with open("videos_for_parsing.csv", "r", encoding='utf-8') as f_obj:
+    with open("videos_for_parsing.csv", "r+", encoding='utf-8') as f_obj:
         csv_dict_reader(f_obj)
